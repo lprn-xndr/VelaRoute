307a0a63d0768e54293c0bdb74683d462982ff19
XNDR <xndrlprn17@proton.me>
2026-04-05 00:37:09 -0400

    chore: add postgres persistence with volumes and config application properties

---
+++ src/main/java/com/xndr/velaroute/VelarouteApplication.java
@@ -0,0 +1,13 @@
+package com.xndr.velaroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelarouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelarouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xndr/velaroute/VelarouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xndr.velaroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelarouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
